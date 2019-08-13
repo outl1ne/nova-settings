11fc66d5d03e0df549468c57968991f9638046a4
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-08-13 12:47:54 +0300

    Add nova_get_setting_value helper function

--- src/helpers.php
+++ src/helpers.php
@@ -8,3 +8,11 @@ if (!function_exists('nova_get_settings')) {
         return Settings::all()->pluck('value', 'key')->toArray();
     }
 }
+
+if (!function_exists('nova_get_setting_value')) {
+    function nova_get_setting_value($key)
+    {
+        $setting = Settings::find($key);
+        return is_null($setting) ? null : $setting->value;
+    }
+}
