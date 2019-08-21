3a4df43117e8c9513f72a251a9457f68bbd8009d
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-08-21 10:51:08 +0300

    Rename migration

--- database/migrations/2019_08_13_000000_create_nova_settings_table.php
+++ database/migrations/2019_08_13_000000_create_nova_settings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSettingsTable extends Migration
+class CreateNovaSettingsTable extends Migration
 {
     /**
      * Run the migrations.
