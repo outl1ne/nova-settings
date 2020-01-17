fdf3457a900d424ee5f2ad31cae115960c7dccd8
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2020-01-17 17:29:33 +0200

    Add improved casting

--- src/Models/Settings.php
+++ src/Models/Settings.php
@@ -4,6 +4,7 @@ namespace OptimistDigital\NovaSettings\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use OptimistDigital\NovaSettings\NovaSettings;
+use Illuminate\Support\Collection as BaseCollection;
 
 class Settings extends Model
 {
@@ -18,13 +19,44 @@ class Settings extends Model
         $this->attributes['value'] = is_array($value) ? json_encode($value) : $value;
     }
 
-    public function getValueAttribute($value)
+    public function getValueAttribute()
     {
-        $customFormatter = NovaSettings::getCustomFormatter();
+        $value = $this->attributes['value'];
+        $casts = NovaSettings::getCasts();
+        $castType = $casts[$this->key] ?? null;
 
-        if (isset($customFormatter)) return call_user_func($customFormatter, $this->key, $value);
+        if (!$castType) return $value;
 
-        return $value;
+        switch ($castType) {
+            case 'int':
+            case 'integer':
+                return (int) $value;
+            case 'real':
+            case 'float':
+            case 'double':
+                return $this->fromFloat($value);
+            case 'decimal':
+                return $this->asDecimal($value, explode(':', $casts[$this->key], 2)[1]);
+            case 'string':
+                return (string) $value;
+            case 'bool':
+            case 'boolean':
+                return (bool) $value;
+            case 'object':
+                return $this->fromJson($value, true);
+            case 'array':
+            case 'json':
+                return $this->fromJson($value);
+            case 'collection':
+                return new BaseCollection($this->fromJson($value));
+            case 'date':
+                return $this->asDate($value);
+            case 'datetime':
+            case 'custom_datetime':
+                return $this->asDateTime($value);
+            case 'timestamp':
+                return $this->asTimestamp($value);
+        }
     }
 
     public static function getValueForKey($key)
