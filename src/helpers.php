5af8d88a68ee61fce84c81ad4e2fa23a135e7d18
Ann <79809955+BeiAduo@users.noreply.github.com>
2023-04-28 16:24:43 -0700

    Update helpers.php
    Update to cache

--- src/helpers.php
+++ src/helpers.php
@@ -3,16 +3,21 @@
 use Outl1ne\NovaSettings\NovaSettings;
 
 if (!function_exists('nova_get_settings')) {
-    function nova_get_settings($settingKeys = null, $defaults = [])
+    function nova_get_settings(array $settingKeys = null,bool $cacheTime=0, $defaults = [])
     {
-        return NovaSettings::getSettings($settingKeys, $defaults);
+        return Cache::remember('nova_settings_'.implode('_', $settingKeys), $cacheTime, function () {
+            return NovaSettings::getSettings($settingKeys, $defaults);
+        }
     }
 }
 
 if (!function_exists('nova_get_setting')) {
-    function nova_get_setting($settingKey, $default = null)
+    function nova_get_setting($settingKey,bool $cacheTime=0, $default = null)
     {
+         return Cache::remember('nova_settings_'.implode('_', $settingKeys), $cacheTime, function () {
         return NovaSettings::getSetting($settingKey, $default);
+        }
+                                
     }
 }
 
